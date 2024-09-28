6a2950fc7af42d6dd8a22cd9d71495f4f2465454
Bruno Alla <alla.brunoo@gmail.com>
2024-09-28 13:06:26 -0300

    Create a minimal Django test project

---
+++ tests/settings.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+SECRET_KEY = "NOTASECRET"  # noqa S105
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "ATOMIC_REQUESTS": True,
+    },
+}
+
+USE_TZ = True
+TIME_ZONE = "UTC"
+
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.admin",
+    "django.contrib.contenttypes",
+    "rest_framework",
+    "tests.testapp",
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "drf_excel.renderers.XLSXRenderer",
+    ),
+}
